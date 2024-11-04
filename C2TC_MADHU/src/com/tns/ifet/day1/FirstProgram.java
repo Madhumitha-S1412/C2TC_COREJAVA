1a47800a6b367339061c9d7230a49e2f9604aedd
shivani <shivani@MahiShivani>
2024-11-04 12:53:53 +0530

    hi

---
+++ C2TC_MADHU/src/com/tns/ifet/day1/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.tns.ifet.day1;
+
+public class FirstProgram {
+public static void main(String[] arg) {
+System.out.println("hello world");
+}
+}
---
+++ C2TC_MADHU/src/com/tns/ifet/day1/Hi.java
@@ -0,0 +1,9 @@
+package com.tns.ifet.day1;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
